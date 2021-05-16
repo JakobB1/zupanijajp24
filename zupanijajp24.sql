4f81990e7ba3103566ebf23140e95eab5a067797
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-05-16 11:09:13 +0100

    table zupanija

--- zupanijajp24.sql
+++ zupanijajp24.sql
@@ -1,3 +1,9 @@
 drop database if exists zupanijajp24;
 create database zupanijajp24;
 use zupanijajp24;
+
+create table zupanija(
+sifra int not null primary key auto_increment,
+naziv varchar(50),
+zupan int
+);
