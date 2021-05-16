6cd87c88b0dd349e62a62b87e1e7c1d1cf5e7528
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-05-16 11:15:18 +0100

    alter tables

--- zupanijajp24.sql
+++ zupanijajp24.sql
@@ -25,3 +25,9 @@ sifra int not null primary key auto_increment,
 ime varchar(50),
 prezime varchar(50)
 );
+
+alter table opcina add foreign key (zupanija) references zupanija(sifra);
+
+alter table mjesto add foreign key (opcina) references opcina(sifra);
+
+alter table zupanija add foreign key (zupan) references zupan(sifra);
