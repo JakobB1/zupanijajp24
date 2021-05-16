f5135b8e09466e6e42bcccd3ec26a163cbe79e40
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-05-16 11:42:20 +0100

    insert mjesto

--- zupanijajp24.sql
+++ zupanijajp24.sql
@@ -26,11 +26,14 @@ ime varchar(50),
 prezime varchar(50)
 );
 
+
+
 alter table opcina add foreign key (zupanija) references zupanija(sifra);
 
 alter table mjesto add foreign key (opcina) references opcina(sifra);
 
 alter table zupanija add foreign key (zupan) references zupan(sifra);
+
 
 
 select * from zupanija;
@@ -39,13 +42,24 @@ insert into zupanija(naziv)
 values('Zagrebacka zupanija'),('Osjecko baranjska'),('Splitsko dalmatinska');
 
 
+
 select * from opcina;
 
 insert into opcina(zupanija,naziv)
 values (1,'Zagreb'),(1,'Velika Gorica'),(2,'Osijek'),(2,'Dakovo'),(3,'Split'),(3,'Kastela');
 
 
+
 select * from zupan;
 
 insert into zupan(ime,prezime)
 values ('Marko','Markovic'),('Ivan','Ivanovic'),('Karlo','Karlovic');
+
+
+
+select * from mjesto;
+
+insert into mjesto(opcina,naziv)
+values (1,'Cerje'),(1,'Gajec'),(2,'Trnje'),(2,'Velika Buna'),
+       (3,'Bilje'),(3,'Cepin'),(4,'Kusevac'),(4,'Durdanci'),
+       (5,'Trstenik'),(5,'Lovret'),(6,'Luksic'),(6,'Gomilica');
